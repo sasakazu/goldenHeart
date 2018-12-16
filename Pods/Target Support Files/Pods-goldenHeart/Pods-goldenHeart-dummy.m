cfa60bffaa94d7b20bef7de3550bc6ed9d6650a4
笹倉一也 <>
2018-12-16 18:49:24 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-goldenHeart/Pods-goldenHeart-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_goldenHeart : NSObject
+@end
+@implementation PodsDummy_Pods_goldenHeart
+@end
---
+++ goldenHeart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goldenHeart
+//
+//  Created by 笹倉一也 on 2018/12/16.
+//  Copyright © 2018 笹倉一也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
